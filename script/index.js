fbc5c33fb88765b893cd23f81b34f17ad1f65968
Great Ogheneochuko <greatochuko123@gmail.com>
2023-11-23 08:24:11 +0100

    finished assignment

--- script/index.js
+++ script/index.js
@@ -1,6 +1,10 @@
 const header = document.querySelector("header");
 const mobileNav = document.querySelector(".mobile-nav");
 const menuBtn = document.querySelector(".menu-btn");
+const stats = document.querySelectorAll(".stat");
+const milestones = document.querySelectorAll(".milestone");
+const partners = document.querySelectorAll(".partners .flex-item img");
+const cart = document.getElementById("cart");
 
 gsap.to(header, {
   duration: 0.5,
@@ -13,3 +17,187 @@ function toggleOpenMobileNav() {
     menuBtn.classList.toggle("show");
   }, 200);
 }
+
+gsap.from(".hero .text h2", {
+  opacity: 0,
+  y: 50,
+  duration: 0.7,
+  delay: 0.4,
+  scrollTrigger: {
+    trigger: "#myElement",
+    start: "top 80%",
+    end: "top 30%",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".hero .text h1", {
+  opacity: 0,
+  y: 50,
+  duration: 0.7,
+  delay: 0.6,
+
+  scrollTrigger: {
+    trigger: "#myElement",
+    start: "top 80%",
+    end: "top 30%",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".hero .text p", {
+  opacity: 0,
+  y: 50,
+  duration: 0.7,
+  delay: 0.8,
+  scrollTrigger: {
+    trigger: "#myElement",
+    start: "top 80%",
+    end: "top 30%",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".hero .images img:first-child", {
+  delay: 0.6,
+  scale: 0.5,
+  opacity: 0,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: ".hero .images",
+    start: "top 90%",
+    end: "top 30%",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".hero .images img:nth-child(2)", {
+  delay: 0.6,
+  scale: 0.5,
+  opacity: 0,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: ".hero .images",
+    start: "top 90%",
+    end: "top 30%",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from(".hero .images img:nth-child(3)", {
+  delay: 0.6,
+  scale: 0.5,
+  opacity: 0,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: ".hero .images",
+    start: "top 90%",
+    end: "top 30%",
+    toggleActions: "play none none none",
+  },
+});
+
+let statDelay = 0;
+stats.forEach((stat) => {
+  gsap.from(stat, {
+    opacity: 0,
+    scale: 0.8,
+    duration: 0.2,
+    delay: statDelay,
+    scrollTrigger: {
+      trigger: stat,
+      start: "top 80%",
+      end: "top 30%",
+      toggleActions: "play none none none",
+    },
+  });
+  statDelay += 0.15;
+});
+
+gsap.from("section.about .image", {
+  scale: 0.9,
+  opacity: 0,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: "section.about .image",
+    start: "top 80%",
+    end: "top 30%",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from("section.mission .image", {
+  scale: 0.9,
+  opacity: 0,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: "section.mission .image",
+    start: "top 80%",
+    end: "top 30%",
+    toggleActions: "play none none none",
+  },
+});
+
+milestones.forEach((milestone) => {
+  gsap.from(milestone, {
+    scale: 0.9,
+    opacity: 0,
+    duration: 0.5,
+    scrollTrigger: {
+      trigger: milestone,
+      start: "top 80%",
+      end: "top 30%",
+      toggleActions: "play none none none",
+    },
+  });
+});
+
+partners.forEach((partner) => {
+  gsap.from(partner, {
+    y: 80,
+    opacity: 0,
+    duration: 0.5,
+    scrollTrigger: {
+      trigger: partner,
+      start: "top 90%",
+      end: "top 30%",
+      toggleActions: "play none none none",
+    },
+  });
+});
+
+gsap.from(".review-articles", {
+  y: 70,
+  opacity: 0,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: ".review-articles",
+    start: "top 80%",
+    end: "top 30%",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from("footer .download-app", {
+  opacity: 0,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: "footer .download-app",
+    start: "top 80%",
+    end: "top 30%",
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.from("footer .nav-section", {
+  opacity: 0,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: "footer .nav-section",
+    start: "top 80%",
+    end: "top 30%",
+    toggleActions: "play none none none",
+  },
+});
+
+console.log(cart);
